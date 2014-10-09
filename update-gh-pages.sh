fa311747f9248e8a5f194d84b0d03e96fcaceb94
Aaron Meurer <asmeurer@gmail.com>
2014-10-09 10:41:50 -0500

    Try not hiding output for failing command

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -109,7 +109,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo -e "Committing"
         git commit -am "Update website after building $TRAVIS_BUILD_NUMBER"
         echo -e "Pushing commit"
-        git push -q origin_token gh-pages > /dev/null 2>&1
+        git push -q origin_token gh-pages
     fi
 fi
 
