d7b118e4b4407aa14f8a1c9cd39bfed5b1d9e5f2
Aaron Meurer <asmeurer@gmail.com>
2014-10-09 12:15:39 -0500

    Commit the right directory for docs

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -94,7 +94,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo -e "Moving built docs into place"
         rm -rf website/docs
         cp -R docs/build/html website/docs
-        git add -A docs/
+        git add -A website/docs/
 
         echo -e "Committing"
         git commit -am "Update docs after building Travis build $TRAVIS_BUILD_NUMBER"
