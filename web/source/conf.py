8e807ccadbba8ce9a764a91f83a7d135f27efb80
Will Warner <electronwill@gmail.com>
2017-01-10 15:06:36 -0600

    2017
    2017

--- web/source/conf.py
+++ web/source/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Conda'
-copyright = '2016, Continuum Analytics'
+copyright = '2017, Continuum Analytics'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
