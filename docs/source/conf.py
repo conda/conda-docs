ad5ac6d2610f31c7f7638a0986c142fb5bffacc0
soapy1 <scastellarin@anaconda.com>
2019-01-17 11:24:27 -0600

    Don't includee numpydoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.inheritance_diagram',
     # 'sphinx.ext.napoleon',
-    'numpydoc',
+#    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
