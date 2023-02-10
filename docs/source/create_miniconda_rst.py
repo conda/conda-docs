f179089713b132c66d00fc121db3a275f0615ec7
Paul Yim <paul.j.yim@gmail.com>
2023-02-10 17:18:05 -0500

    Make sure win-32 rows don't get dropped and remain pinned to v4.12.0; also update template to facilitate 'Latest' line

--- docs/source/create_miniconda_rst.py
+++ docs/source/create_miniconda_rst.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python
-""" create miniconda.rst file with size and sha256 for latest installers """
+""" 
+Create miniconda.rst file with size and sha256 for latest installers.
+See the miniconda.rst.jinja2 for the template that is ultimately rendered.
+
+NOTE: Please make sure to update the global variables below:
+- MINICONDA_VERSION
+- PYTHON_VERSION
+- RELEASE_DATE
+
+NOTE: Also confirm the PLATFORM_MAP is up-to-date.
+"""
 
 import urllib.request
 import json
@@ -10,9 +20,15 @@ OUT_FILENAME = "miniconda.rst"
 TEMPLATE_FILENAME = "miniconda.rst.jinja2"
 FILES_URL = "https://repo.anaconda.com/miniconda/.files.json"
 
-CONDA_VERSION = "23.1.0-1"
+# Update these!
+MINICONDA_VERSION = "23.1.0-1"
+PYTHON_VERSION = "3.10.9"  # This is the version of Python that's bundled into the Miniconda installers.
+RELEASE_DATE = "February 7, 2023"
+
+# Confirm these are up-to-date.
 PLATFORM_MAP = {
     "win64": "Windows-x86_64.exe",
+    "win32": "Windows-x86.exe",
     "osx64_sh": "MacOSX-x86_64.sh",
     "osx64_pkg": "MacOSX-x86_64.pkg",
     "osx_arm64_sh": "MacOSX-arm64.sh",
@@ -33,30 +49,46 @@ def sizeof_fmt(num, suffix="B"):
 
 
 def get_latest_miniconda_sizes_and_hashes():
+    """
+    Returns a dict with sizes and SHA256 hashes for each
+    installer built for the latest CONDA_VERSION.
+    """
     with urllib.request.urlopen(urllib.request.Request(url=FILES_URL)) as f:
         data = json.loads(f.read().decode("utf-8"))
 
     info = {
-        "conda_version": CONDA_VERSION
+        "miniconda_version": MINICONDA_VERSION,
+        "conda_version": MINICONDA_VERSION.split("-")[0],
+        "python_version": PYTHON_VERSION,
+        "release_date": RELEASE_DATE
     }
+
     for platform_id, installer_suffix in PLATFORM_MAP.items():
-        latest_installer = 'Miniconda3-latest-{}'.format(installer_suffix)
-        info["{}_py3_latest_size".format(platform_id)] = sizeof_fmt(data[latest_installer]['size'])
-        info["{}_py3_latest_hash".format(platform_id)] = data[latest_installer]['sha256']
+        latest_installer = f"Miniconda3-latest-{installer_suffix}"
+        info[f"{platform_id}_py3_latest_size"] = sizeof_fmt(data[latest_installer]["size"])
+        info[f"{platform_id}_py3_latest_hash"] = data[latest_installer]["sha256"]
         for py_version in ("37", "38", "39", "310"):
-            full_installer = 'Miniconda3-py{}_{}-{}'.format(py_version, CONDA_VERSION, installer_suffix)
+            full_installer = f"Miniconda3-py{py_version}_{MINICONDA_VERSION}-{installer_suffix}"
+
+            # win-32 is and will remain at "frozen" at v4.12.0 
+            # (we no longer make new builds for this subdir)
+            if platform_id == "win32":
+                full_installer = f"Miniconda3-py{py_version}_4.12.0-{installer_suffix}"
+
             if full_installer not in data:
                 continue
-            info["{}_py{}_size".format(platform_id, py_version)] = sizeof_fmt(data[full_installer]['size'])
-            info["{}_py{}_hash".format(platform_id, py_version)] = data[full_installer]['sha256']
+            info[f"{platform_id}_py{py_version}_size"] = sizeof_fmt(data[full_installer]["size"])
+            info[f"{platform_id}_py{py_version}_hash"] = data[full_installer]["sha256"]
 
     return info
 
 
 def main():
     rst_vars = get_latest_miniconda_sizes_and_hashes()
+
     with open(TEMPLATE_FILENAME) as f:
         template_text = f.read()
+
     template = Template(template_text)
     rst_text = template.render(**rst_vars)
     with open(OUT_FILENAME, 'w') as f:
