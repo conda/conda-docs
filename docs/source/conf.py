cf3db0247f85b66cd51f9c3a348d2e885d323bf1
Paul Yim <paul.j.yim@gmail.com>
2023-02-10 18:17:30 -0500

    Update copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'conda'
-copyright = u'2017, Anaconda, Inc.'
+copyright = u'2023, Anaconda, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
