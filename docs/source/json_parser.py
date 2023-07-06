cd646550910a50d3dba91d95ec9bbd5cb332ae1b
Jaida Rice <jaidarice@gmail.com>
2023-07-06 09:07:30 -0700

    Iterate through miniconda_releases dir
    Co-authored-by: Marco Esters <marcoesters@users.noreply.github.com>

--- docs/source/json_parser.py
+++ docs/source/json_parser.py
@@ -2,39 +2,50 @@ import json
 from pathlib import Path
 
 SOURCE_DIR = Path(__file__).parent
-info_json_file_path = SOURCE_DIR / "_tmp/" / "info.json"
+RELEASE_DIR = SOURCE_DIR / "miniconda_releases"
 
-# To get all data needed for the release notes of one release,
-# we need to call `get_installer_information` for every _info.json
-# inside the release directory
 
-def get_installer_info(info_json_file_path: Path) -> dict:
+def get_installer_info(release: Path) -> dict:
     """
-    Process _info.json file output by constructor to get installer information,
+    Process _info.json files output by constructor to get installer information,
     including the list of delivered packages
     """
-    info_dict = json.loads(info_json_file_path.read_text())    
-    info_dict["packages"] = [dist_str_to_dict(x) for x in info_dict["_dists"]]
-
-    return info_dict
-
-
-def dist_str_to_dict(dist: str) -> dict:
+    def get_package_list(dists: list[str]) -> list:
         #  The `dist` strings are of the format "<name>-<version>-<hash>_<build_num>.conda"
+        packages = []
+        for dist in dists:
+            pkg_name, pkg_version, pkg_split = dist[:-6].rsplit("-", 2)
+            pkg_hash = pkg_split.split("_")[0]
+            pkg_build_num = pkg_split.split("_")[1]
+    
+            package = {
+                "name": pkg_name,
+                "version": pkg_version,
+                "hash": pkg_hash,
+                "build_num": pkg_build_num
+            }
+            packages.append(package)
+        return packages
 
-        pkg_name, pkg_version, pkg_split = dist[:-6].rsplit("-", 2)
-        pkg_hash = pkg_split.split("_")[0]
-        pkg_build_num = pkg_split.split("_")[1]
+    installer_info = {}
+    for info_json in release.iterdir():
+        if not info_json.name.endswith("_info.json"):
+            continue
+        info_dict = json.loads(info_json.read_text())
+        info_dict["packages"] = get_package_list(info_dict["_dists"])
+        platform = info_dict["_platform"]
+        py_version = info_dict["version"].split("_")[0]
+        installer_type = info_dict["installer_type"]
+        installer_info[platform,py_version,installer_type] = info_dict
 
-        return {
-            "name": pkg_name,
-            "version": pkg_version,
-            "hash": pkg_hash,
-            "build_num": pkg_build_num
-        }
+    return installer_info
+
 
 def main():
-    print(get_installer_info(info_json_file_path))
+    release_info = {}
+    for release in RELEASE_DIR.iterdir():
+        release_info[release.name] = get_installer_info(release)
+    print(release_info)
 
 if __name__ == "__main__":
      main()
